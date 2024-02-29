61067434164149f5a75cff3aa84afd0dad5a9f83
himu <satirtharoy2003@gmail.com>
2024-02-29 14:39:14 +0600

    good phone for your daily use

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -13,7 +13,7 @@ const Cards = () => {
         </figure>
         <div className="card-body items-center text-center">
           <h2 className="card-title">{phone.phone_name}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <p>Good phone for your daily use.</p>
           <div className="card-actions">
             {/* <button className="btn btn-primary">Show Details</button> */}
             <Modal id = {phone.slug}/>
